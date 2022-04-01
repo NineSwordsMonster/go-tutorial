920576c4893544446697eae94e31bef0a354d3b8
wangjia <wangjia@patsnap.com>
2022-04-01 18:27:32 +0800

    add-a-test

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ cd hello
 go mod edit -replace github.com/NineSwordsMonster/go-tutorial/greetings=../greetings  
 go mod tidy 
 ```
+
+```shell
+cd ../greetings/
+go test
+go test -v
+
+```
---
+++ greetings/greetings_test.go
@@ -0,0 +1,24 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+
+	message, err := Hello("Gladys")
+	if !want.MatchString(message) {
+		t.Fatalf(`Hello("Gladys"') = %q,%v, want match for %#q, nil `, message, err, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
